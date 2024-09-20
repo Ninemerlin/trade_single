79d8bbb18d62e5dc5496c3289d875601f1f91c6c
Zincoid <2660181154@qq.com>
2024-09-20 18:25:58 +0800

    Zincoid

---
+++ src/main/java/com/dlut/tradesys/common/pojo/Spec.java
@@ -0,0 +1,21 @@
+package com.dlut.tradesys.common.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("spec")
+public class Spec{
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    private Long itemId;
+    private String name;
+    private Integer price;
+    private Integer stock;
+}
--- src/main/resources/SQL/spec.sql
+++ src/main/resources/SQL/spec.sql
@@ -7,7 +7,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `spec`;
 CREATE TABLE `spec`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `item_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `item_id` bigint NOT NULL COMMENT '商品id',
   `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
   `price` int NOT NULL DEFAULT '0' COMMENT '价格',
   `stock` int UNSIGNED NOT NULL COMMENT '库存数量',
